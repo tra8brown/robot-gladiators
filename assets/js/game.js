d04e0a0d57d7207f13900146c033ab7601a28fba
TraBee <tra8brown@gmail.com>
2021-05-04 13:58:28 -0600

    Updated in games.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?");
+//what is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("our robot's name is" + playerName);
+
+//this creates a function name "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+
+// fight();
